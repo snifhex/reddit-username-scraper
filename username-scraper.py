cde8e8c17d8ad3226bc8f8db719123280ff5099b
Sachin Tripathi <sachintripathis84@gmail.com>
2019-06-23 16:45:31 +0530

    incomplete

---
+++ username-scraper.py
@@ -0,0 +1,73 @@
+import os
+import csv
+import praw
+import json
+
+usernameList = []
+stats = {}
+
+def auth():
+    with open('credentials.json', 'r') as json_obj:
+        credentials = json_obj.read()
+    credentials = json.loads(credentials)
+    print(credentials)
+    username = credentials['username']
+    password = credentials['password']
+    client = credentials['client_id']
+    secret = credentials['client_secret']
+    agent = credentials['user_agent']
+    reddit = praw.Reddit( username=username, password=password, 
+            client_id=client, client_secret=secret, user_agent=agent)
+    return reddit
+
+def writer():
+    if os.path.isfile('usernames.csv'):
+        with open('usernames.csv', 'a') as doc:
+            writer = csv.writer(doc)
+            for username in usernameList:
+                writer.writerow((username,))
+    else:
+        with open('usernames.csv', 'w') as doc:
+            writer = csv.writer(doc)
+            writer.writerow(('username',))
+            for username in usernameList:
+                writer.writerow((username,))
+
+def statWriter():
+    if os.path.isfile('stat.csv'):
+        with open('stat.csv', 'a') as doc:
+            writer = csv.writer(doc)
+            for key, values in stats.items():
+                writer.writerow((key, values))
+    else:
+        with open('stat.csv', 'w') as doc:
+            writer = csv.writer(doc)
+            for key, values in stats.items():
+                writer.writerow(('pages', 'usernames'))
+    
+        
+def getUsernames(reddit):
+    page = 0 
+    for submission in reddit.front.hot():
+        page += 1
+        count = 0
+        if submission.author not in usernameList:
+            usernameList.append(submission.author)
+        for comment in submission.comments:
+            try:
+                if comment.author != 'None' and comment.author not in usernameList:
+                    usernameList.append(comment.author)    
+                count += 1
+            except:
+                break
+        stats[page] = count
+        print(str(page)+ ":" +str(count))
+
+def main():
+    reddit = auth()
+    getUsernames(reddit)
+    writer()
+    statWriter()
+
+if __name__ == '__main__':
+    main()
